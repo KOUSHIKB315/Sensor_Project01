9cfbd770c1aa10b82b5c1649f882953d90aa6aa1
Koushik <koushikbanerjee72628@gmail.com>
2026-05-11 21:02:36 +0530

    this is just for

--- src/exception.py
+++ src/exception.py
@@ -4,7 +4,7 @@ def error_message_detail(error, error_detail: sys):
   _,_,exc_tb = error_detail.exc_info() # exc_info() is a function in the sys module that returns a tuple containing information about the current exception being handled. The tuple contains three values: the exception type, the exception value, and the traceback object. By unpacking the tuple into three variables, we can access the specific details of the exception that occurred. exc_tb is the traceback object that contains information about the call stack at the point where the exception was raised. This can be useful for debugging purposes, as it allows us to see the sequence of function calls that led to the error.
   
   file_name = exc_tb.tb_frame.f_code.co_filename  # tb_frame is an attribute of the traceback object that represents the current stack frame. f_code is an attribute of the frame object that contains information about the code being executed in that frame. co_filename is an attribute of the code object that contains the name of the file where the code is located. By accessing these attributes, we can retrieve the name of the file where the exception occurred, which can be helpful for debugging and understanding the context of the error.
-  error_message =f"Error occured python script name{0} line number[{1}] error message[{2}]".format(
+  error_message =f"Error occured python script name{0} line number[{1}] error message[{2}]". format(
     file_name, exc_tb.tb_lineno, str(error)
   )
   return error_message
